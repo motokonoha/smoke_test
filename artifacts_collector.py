cdfe5491fb26b17cd2977d33fb33c75a76478186
Tan Chun Mun <vcfr67@motorolasolutions.com>
2015-06-30 15:59:40 +0800

    changed the file comparison using json

--- artifacts_collector.py
+++ artifacts_collector.py
@@ -5,6 +5,7 @@ import subprocess
 import sys
 import re
 from jenkins_utils import *
+import json
 
 
 class artifacts_collection(base):
@@ -16,12 +17,16 @@ class artifacts_collection(base):
             print(self.CP_DIRS)
             new_set = get_cur_files_set(self.MONITOR_Z19_FILES, self.MONITOR_FILES, self.STATIC_FILES, self.MAIN_DIR, self.CP_DIRS)
             old_set = load_files_set(self.DUMP_FILE)
-            fail_desc = are_files_set_same(old_set, new_set)
+
+            new_set_json = json.dumps(new_set)
+            old_set_json = json.dumps(old_set)
+            fail_desc = new_set_json != old_set_json
+
             if not fail_desc:
-                #print("Same - do not trigger")
+                print("Same - do not trigger")
                 return False
             else:
-                #print("Different - trigger")
+                print("Different - trigger")
                 #description_tag = "description"
                 #cause_tag = "cause"
                 #m = re.search("(\d{4}.?)$",new_set[0])
@@ -43,13 +48,13 @@ if __name__ == "__main__":
     artifacts_collector = artifacts_collection()
     is_latest = True
     # will only verify version if dump not appear
-    if not os.path.exists(artifacts_collector.LOCAL_DUMP_FILE):
-        # This for automated test station
-        if artifacts_collector.is_automated():
-            print(" >>>> verify version for automated test\n")
-            is_latest = artifacts_collector.verify_version()
-        else:
-            artifacts_collector.verify_version()
+    # This for automated test station
+    if artifacts_collector.is_automated():
+        print(" >>>> verify version for automated test\n")
+        is_latest = artifacts_collector.verify_version()
+    else:
+        print(" >>>> No Automation required\n")
+        artifacts_collector.verify_version()
     if is_latest:
         if artifacts_collector.require_flash():
             print(" >>>> collecting artifacts to %s\n"%artifacts_collector.LOCAL_ARTIFACTS_LOCATION)
