8894c053e3bea608a46b4988635bdfd47ca333da
TRWD74 <jiunngim55@gmail.com>
2015-07-28 11:52:34 +0800

    Generate CPV file with random uuid

--- test_execution.py
+++ test_execution.py
@@ -13,15 +13,26 @@ from multiprocessing import Pool
 import fnmatch
 
 from ms import *
-
+from create_cpv import *
 
 
 class test_management(base):
-   def generate_xml(self):
-       pass
+   def __init__(self):
+       self.run_test = None
+
+   def generate_cpv(self):
+        try:
+            self.run_test = create_cpv()
+            self.run_test.rand_UUID()
+            self.run_test.create_cpv_file()
+        except:
+            print(("[ERROR] %s")%sys.exc_info()[1])
+            exit(-1)
+
 
    def flash_cpv(self):
-       pass
+       if self.run_test:
+           filename = self.run_test.get_cpv_name()
 
    def execute_test(self):
        pass
@@ -31,7 +42,7 @@ class test_management(base):
 
 if __name__ == "__main__":
     test_executor = test_management()
-    test_executor.generate_xml()
+    test_executor.generate_cpv()
     test_executor.flash_cpv()
     test_executor.execute_test()
     exit(0)
