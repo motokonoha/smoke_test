5a256c1b88b88e2000e9f2eaa2da1d6ce8121427
Tan Chun Mun <vcfr67@motorolasolutions.com>
2015-08-10 17:49:18 +0800

    added arguments to trigger test run

--- main.py
+++ main.py
@@ -3,16 +3,47 @@ __author__ = 'vcfr67'
 import subprocess
 import time
 import os
-import sys
+import sys, getopt
+import pprint
+
+opts = []
+args = []
+def get_arg_value_by_opt(option):
+    arguments = []
+    for opt, arg in opts:
+        if opt == option:
+            arguments.append(arg)
+    return arguments
+
+def set_arg_options(arguments, short_options, long_options ):
+        global opts
+        global args
+        try:
+            opts, args = getopt.getopt(arguments, short_options, long_options)
+        except getopt.GetoptError:
+            print(getopt.GetoptError.msg)
+            sys.exit(2)
 
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.realpath(__file__))
-    processes = [
-        os.path.join(script_dir, "artifacts_collector.py"),
-        os.path.join(script_dir, "gather_test.py"),
-        os.path.join(script_dir, "flash_manager.py"),
-        os.path.join(script_dir, "test_execution.py")
-    ]
+    set_arg_options(sys.argv[1:], 'p:', ['process='])
+    args = get_arg_value_by_opt('-p')
+    args += get_arg_value_by_opt('--process')
+
+    processes = []
+
+    pprint.pprint(args)
+    if len(args) > 0:
+        for process_name in args:
+            process = os.path.join(script_dir, "%s.py"%(process_name))
+            processes.append(process)
+    else:
+        processes = [
+            os.path.join(script_dir, "artifacts_collector.py"),
+            os.path.join(script_dir, "gather_test.py"),
+            os.path.join(script_dir, "flash_manager.py")
+            # os.path.join(script_dir, "test_execution.py")
+        ]
     for process in processes:
         cmd = ["python", process]
         if len(sys.argv) > 2:
